8c6db25db94c0f193235328e5c6bc2541a69c9c2
Sakthivel J <98632696+sakthivelgreen@users.noreply.github.com>
2024-12-03 16:21:29 +0530

    !fix: Goal 1

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ document.addEventListener("DOMContentLoaded", function () {
     const headerColumn = document.querySelector('.sticky.col-title');
 
     if (firstColumn && secondColumn) {
-        // Get the width of the first sticky column
+        firstColumn.forEach(item => { item.style.padding = '16px 30px' })
         const firstStickyWidth = firstColumn[0].offsetWidth;
-        // Set the left property of the second sticky column
         headerColumn.style.left = `${firstStickyWidth}px`;
         secondColumn.forEach(item => { item.style.left = `${firstStickyWidth}px` });
     }
+
+    const tableContainer = document.querySelector('.container');
+    const cells = document.querySelectorAll('td:not(.sticky), th:not(.sticky)');
+
+    tableContainer.addEventListener('scroll', () => {
+
+        cells.forEach(cell => {
+            const cellRect = cell.getBoundingClientRect();
+
+            if (cellRect.left - tableContainer.getBoundingClientRect().left <= 50) {
+                cell.style.opacity = 0;
+            } else {
+                cell.style.opacity = 1;
+            }
+
+        });
+    });
+
 });
